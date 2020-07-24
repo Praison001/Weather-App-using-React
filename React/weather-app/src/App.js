12e2ded9658ca2a16a10afec29b2eced3bbb5312
Praison001 <60835238+Praison001@users.noreply.github.com>
2020-07-24 12:25:21 +0530

    weather

---
+++ React/weather-app/src/App.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import Weather from './component/weather.component'
+import Form from './component/form.component'
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const api_key= "9c7bc722cc89dd73b40ab374c3aea1af"
+
+
+class App extends React.Component{
+  constructor(){
+    super()
+    this.state= {
+      city:undefined,
+      country:undefined,
+      celsius:undefined,
+      temp_max:undefined,
+      temp_min:undefined,
+      description:"",
+      error:false
+    }
+  }
+//converting the temperature to celsius
+  calCelsius(temp){
+    let cel= Math.floor(temp-273.15)
+    return cel
+  }
+
+  getWeather= async(e) => {
+
+    e.preventDefault()
+
+    const city= e.target.elements.city.value
+    const country= e.target.elements.country.value
+
+    if (city&&country){
+      const api_call= await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}uk&appid=${api_key}`)
+    const response= await api_call.json()
+    //see the data fetched from the api
+    console.log(response)
+
+    this.setState({
+      city: `${response.name},`,
+      country: `${response.sys.country}`,
+      celsius: this.calCelsius(response.main.temp),
+      temp_max: this.calCelsius(response.main.temp_max),
+      temp_min: this.calCelsius(response.main.temp_min),
+      description: response.weather[0].description,
+      error:false
+    })
+
+    }else{
+      this.setState({error:true})
+    }
+  }
+
+
+  render(){
+    return(
+      <div className="App">
+
+        <Form loadweather={this.getWeather} error={this.state.error} />
+        <Weather 
+        city={this.state.city} 
+        country={this.state.country} 
+        temp_celsius={this.state.celsius}
+        temp_max= {this.state.temp_max}
+        temp_min= {this.state.temp_min}
+        description= {this.state.description}
+        />
+        
+      </div>
+    )
+  }
+}
+
+export default App
